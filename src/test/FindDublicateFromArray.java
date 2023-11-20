58f77b94ae17ef203112d0e4046aae91591a15c0
BlvckDeamon <orscoutop100@gmail.com>
2023-11-20 21:04:14 +0530

    101

--- src/test/FindDublicateFromArray.java
+++ src/test/FindDublicateFromArray.java
@@ -1,9 +1,18 @@
 package test;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class FindDublicateFromArray {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		List<Integer> asList = Arrays.asList(1,2,3,4,2,3,5,7,8);
+		
+		Set<Integer> set = new TreeSet<>();
+		asList.stream().filter(e -> !set.add(e)).forEach(System.out::println);
 
 	}
 
